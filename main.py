a9f89fb6064827f04f2ddf8273a8b22b44ff27f6
wardonne <Wardonne@yandex.ru>
2023-12-19 02:08:41 +0400

    add filters

--- main.py
+++ main.py
@@ -23,9 +23,21 @@ def count_word(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     df['count_word'] = df['review'].apply(lambda word: len(word.split()))
     return df
 
+def filter_by_words(df: pd.core.frame.DataFrame, count_words: int) -> pd.core.frame.DataFrame:
+    return df[df.count_word >= count_words]
+
+
+def filter_by_rating(df: pd.core.frame.DataFrame, count_rating: int) -> pd.core.frame.DataFrame:
+    return df[df.rating == count_rating]
+
 
 if __name__ == '__main__':
     path = r"/Users/wardonne/Desktop/Lab_python/PP_3_Lab/annotation1.csv"
     df = read_csv_in_data_frame(path)
     df = count_word(df)
     print(df)
+    df = filter_by_words(df, 1000)
+    print(df)
+    df = filter_by_rating(df, 1)
+    print(df)
+
